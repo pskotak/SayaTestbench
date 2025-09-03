5157d5ff78484daefb086772961b2ec7f468f68a
pskotak <eltec@eltecweb.cz>
2025-09-03 19:39:23 +0200

    feat: Add local map

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <unistd.h>
+#include <thread>
 
 #include "vision/vision.h"
+#include "LocalMap/locmap.h"
+
+//#define DispVision
 
 #define maxdist 4.0f
 #define distpixel 0.006f
 #define matlenpx 800
 
 #define hrange 2.0f
+
+#define GridUpscale 4.0
 
 // ============================================================================
 int main(int argc, char **argv) {
@@ -24,18 +30,29 @@ int main(int argc, char **argv) {
     
     bool ShowPoints = true;
     
+    std::thread LocMapThread;
+    cv::Mat show_grid(GridCells,GridCells,CV_8UC3,cv::Scalar(127,127,127));
+    cv::Mat up_grid;
+    
+// ----------------------------------------------------------------------------    
     std::cout << "Saya the robot Test bench" << std::endl;
+    //vision::GetSerNo();
+    //return 0;
     
 // ----------------------------------------------------------------------------
     vision::StartPcRow = 160;
     vision::EndPcRow = D455H-60; //vision::EndPcRow = D455H-160;
     vision::Init();
+    std::cout << "Vision running." << std::endl;
+    
+    locmap::ObstacleDelta = 0.15;
+    LocMapThread = std::thread(&locmap::RunLocMap);
     
     while (!QuitProgram) {
         vision::Frame();
         if (vision::NewD455) {
-            cv::imshow("RGB",vision::RGB_image);
-            
+            //cv::imshow("RGB",vision::RGB_image);
+#ifdef DispVision
             vision::depth_image16.convertTo(processed_frame,CV_8U,255.0 / D455depth_color_max,0.0);
             cv::applyColorMap(processed_frame,processed_frame,cv::COLORMAP_JET);
             
@@ -48,9 +65,39 @@ int main(int argc, char **argv) {
             pc = vision::PointCloud; // deep copy
             //std::cout << vision::ScPoints.size() << std::endl;
             sc = vision::ScPoints;
+#endif
+            if (!locmap::UpdateGridMap) {
+                show_grid.setTo(cv::Scalar(127,127,127));
+                for (int y = 0; y < GridCells; ++y) {
+                    for (int x = 0; x < GridCells; ++x) {
+                        if (locmap::ObstacleGrid[x][y].free) {
+                            show_grid.at<cv::Vec3b>(x,y) = {255,255,255};
+                        }
+                        if (locmap::ObstacleGrid[x][y].obstacle) {
+                            show_grid.at<cv::Vec3b>(x,y) = {160,160,80};
+                        }
+                        if (locmap::ObstacleGrid[x][y].occupied) {
+                            show_grid.at<cv::Vec3b>(x,y) = {0,0,0};
+                        }
+                        if (locmap::ObstacleGrid[x][y].path) {
+                            show_grid.at<cv::Vec3b>(x,y) = {180,0,255};
+                        }
+                    }
+                }
+                show_grid.at<cv::Vec3b>(GridCenter,GridCenter) = {255,0,0};                
+                
+                locmap::lmap_depth_image = vision::depth_image16.clone();
+                locmap::UpdateGridMap = true;
+            }
+
             vision::NewD455 = false;
         }
+        
+        cv::resize(show_grid,up_grid,cv::Size(),GridUpscale,GridUpscale,cv::INTER_NEAREST); // resize bez interpolace                
+        cv::imshow("Map",up_grid);
+        
 
+#ifdef DispVision
         ptshow.setTo(cv::Scalar(0,0,0));
         if (ShowPoints) {
             for (int i=0;i<pc.size();i++) {
@@ -86,7 +133,7 @@ int main(int argc, char **argv) {
         ptshow.at<cv::Vec3b>(matlenpx/2,matlenpx/2) = {255,180,0};
         
         cv::imshow("Scan",ptshow);
-        
+#endif        
         
         int key = cv::waitKey(1); // Wait for a key press for 1ms
         if (key == 27) { // ESC key
@@ -109,6 +156,11 @@ int main(int argc, char **argv) {
         }
         
         usleep(100000);
-    }    
+    }
+    
+    std::cout << "down LocalMap" << std::endl;
+    locmap::ShutdownLocMap = true;
+    LocMapThread.join();
+    
     return 0;
 }
